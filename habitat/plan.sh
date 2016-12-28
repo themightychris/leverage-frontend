fb15b09326ffb780b01fac741dc8c94c44b6c01d
Chris Alfano <chris@jarv.us>
2016-12-28 04:08:42 +0000

    Improve comments describing step plans

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -23,12 +23,19 @@ do_unpack() {
 
 
 do_build() {
+  # Copy files from wherever this plan is being run from to the
+  # temporary build space for this package
   cp -vr $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname
 }
 
 do_install() {
+  # Copy the things we want to distribute from the current
+  # directory (this package's temporary build space) to the
+  # root for this package's build
   mkdir -p ${pkg_prefix}/www
   cp -v *.html ${pkg_prefix}/www/
   cp -vr assets ${pkg_prefix}/www/
+
+  # Designate discover.html as the default page
   ln -vs discover.html ${pkg_prefix}/www/index.html
 }
